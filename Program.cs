1c3ae36f4bd2d955099d9162d4b94243ec7d8e36
irwingcampanati <irwingcampanati.cube@gmail.com>
2023-03-16 17:16:17 -0300

    att

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Faça um programa em C# que imprima todos os múltiplos de 3, entre 1 e 100.
+// Para saber se um número é múltiplo de 3, você pode fazer if(numero % 3 == 0).
+
+using System;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
